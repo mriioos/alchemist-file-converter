fa10eb3263c0de4d3c3d1812c9da04f74b562a71
Miguel Ríos Marcos <114981304+mriioos@users.noreply.github.com>
2026-03-22 22:15:23 +0100

    Update entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -10,4 +10,7 @@ apt-get update && apt-get install -y --no-install-recommends \
     fonts-noto \
     && rm -rf /var/lib/apt/lists/*
 
+echo "ttf-mscorefonts-installer msttcorefonts/accepted-msa-eula select true" | debconf-set-selections
+apt-get install -y --no-install-recommends ttf-mscorefonts-installer
+
 exec "$@"
